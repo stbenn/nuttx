499d75cef2a7c1b9541a0fa3c14d79267632b141
stbenn <tbennett@2g-eng.com>
2025-03-31 14:13:44 -0500

    More definitions added.

--- arch/arm/src/stm32h5/stm32h563xx_flash.c
+++ arch/arm/src/stm32h5/stm32h563xx_flash.c
@@ -42,16 +42,74 @@
 #include <errno.h>
 
 #include "hardware/stm32_flash.h"
+#include "hardware/stm32_memorymap.h"
 #include "arm_internal.h"
 
 /****************************************************************************
  * Pre-processor Definitions
  ****************************************************************************/
 
+#define _K(x) ((x)*1024)
+#define FLASH_SECTOR_SIZE _K(8)
+#define FLASH_PAGE_SIZE     16
+
+/* Flash size is known from the chip selection:
+ *
+ *   When CONFIG_STM32H5_FLASH_OVERRIDE_DEFAULT is set the
+ *   CONFIG_STM32H5_FLASH_CONFIG_x selects the default FLASH size based on
+ *   the chip part number.  This value can be overridden with
+ *   CONFIG_STM32H5_FLASH_OVERRIDE_x
+ *
+ *   Parts STM32H552xC and STM32H562xC have 256Kb of FLASH
+ *   Parts STM32H552xE and STM32H562xE have 512Kb of FLASH
+ *
+ *   N.B. Only Single bank mode is supported
+ */
+
+#if !defined(CONFIG_STM32H5_FLASH_OVERRIDE_DEFAULT) && \
+    !defined(CONFIG_STM32H5_FLASH_OVERRIDE_B) && \
+    !defined(CONFIG_STM32H5_FLASH_OVERRIDE_C) && \
+    !defined(CONFIG_STM32H5_FLASH_OVERRIDE_E) && \
+    !defined(CONFIG_STM32H5_FLASH_OVERRIDE_G) && \
+    !defined(CONFIG_STM32H5_FLASH_OVERRIDE_I) && \
+    !defined(CONFIG_STM32H5_FLASH_CONFIG_B) && \
+    !defined(CONFIG_STM32H5_FLASH_CONFIG_C) && \
+    !defined(CONFIG_STM32H5_FLASH_CONFIG_E) && \
+    !defined(CONFIG_STM32H5_FLASH_CONFIG_G) && \
+    !defined(CONFIG_STM32H5_FLASH_CONFIG_I)
+#  define CONFIG_STM32H5_FLASH_OVERRIDE_E
+#  warning "Flash size not defined defaulting to 512KiB (E)"
+#endif
+
+/* Override of the Flash has been chosen */
+
+#if !defined(CONFIG_STM32H5_FLASH_OVERRIDE_DEFAULT)
+#  undef CONFIG_STM32H5_FLASH_CONFIG_C
+#  undef CONFIG_STM32H5_FLASH_CONFIG_E
+#  if defined(CONFIG_STM32H5_FLASH_OVERRIDE_C)
+#    define CONFIG_STM32H5_FLASH_CONFIG_C
+#  elif defined(CONFIG_STM32H5_FLASH_OVERRIDE_E)
+#    define CONFIG_STM32H5_FLASH_CONFIG_E
+#  endif
+#endif
+
+/* Define the valid configuration  */
+
+#define FLASH_KEY1      0x45670123
+#define FLASH_KEY2      0xCDEF89AB
+#define FLASH_OPTKEY1   0x08192A3B
+#define FLASH_OPTKEY2   0x4C5D6E7F
+#define FLASH_OBKKEY1   0x192A083B
+#define FLASH_OBKKEY2   0x5E7F4C5D
+
+#define FLASH_ERASEDVALUE     0xffu
+#define FLASH_ERASEDVALUE_DW  0xffffffffu
+#define FLASH_TIMEOUT_VALUE   5000000   /* 5s */
 /****************************************************************************
  * Private Types
  ****************************************************************************/
 
+// TODO: Is ifbase necessary on H5? H7 has 2 separate base addr for reg per bank
 struct stm32h5_flash_priv_s
 {
   mutex_t  lock;    /* Bank exclusive */
@@ -65,11 +123,33 @@ struct stm32h5_flash_priv_s
  * Private Data
  ****************************************************************************/
 
+/* The STM32H563xx does not have separate register base per bank.*/
+static struct stm32h5_flash_priv_s stm32h5_flash_bank1_priv =
+{
+  .lock     = NXMUTEX_INITIALIZER,
+  .ifbase   = STM32_FLASHIF_BASE,
+  .base     = STM32_FLASH_BANK1,
+  .stblock  = 0,
+  .stpage   = 0,
+};
+#if STM32_DUAL_BANK
+#warning "(In Development) .stblock and .stpage of bank2 not initialized properly."
+static struct stm32h5_flash_priv_s stm32h5_flash_bank2_priv =
+{
+  .lock     = NXMUTEX_INITIALIZER,
+  .ifbase   = STM32_FLASHIF_BASE,
+  .base     = STM32_FLASH_BANK2,
+  .stblock  = 0,
+  .stpage   = 0,
+};
+#endif
 
 /****************************************************************************
  * Private Functions
  ****************************************************************************/
 
+// TODO: Don't think getreg, putreg, and modifyreg are necessary. These are
+// here because the H7 has separate flash registers per bank!
 /****************************************************************************
  * Name: stm32h5_flash_getreg32
  *
@@ -330,6 +410,10 @@ void stm32h5_flash_swapbanks(void)
   #warning "stm32h5_flash_swapbanks() is not implemented."
 }
 
+#ifdef CONFIG_ARCH_HAVE_PROGMEM
+
+/* up_progmem_x functions defined in nuttx/include/nuttx/progmem.h */
+
 size_t up_progmem_pagesize(size_t page)
 {
   return FLASH_PAGE_SIZE;
@@ -403,227 +487,231 @@ size_t up_progmem_erasesize(size_t block)
 
 ssize_t up_progmem_eraseblock(size_t block)
 {
-  struct stm32h5_flash_priv_s *priv;
-  int ret;
-  size_t block_address = STM32_FLASH_BANK1 + (block * FLASH_SECTOR_SIZE);
+  #warning "(In Development) up_progmem_eraseblock not implemented"
+//   struct stm32h5_flash_priv_s *priv;
+//   int ret;
+//   size_t block_address = STM32_FLASH_BANK1 + (block * FLASH_SECTOR_SIZE);
 
-  if (block >= PROGMEM_NBLOCKS)
-    {
-      return -EFAULT;
-    }
+//   if (block >= PROGMEM_NBLOCKS)
+//     {
+//       return -EFAULT;
+//     }
 
-  priv = stm32h5_flash_bank(block_address);
+//   priv = stm32h5_flash_bank(block_address);
 
-  ret = nxmutex_lock(&priv->lock);
-  if (ret < 0)
-    {
-      return (ssize_t)ret;
-    }
+//   ret = nxmutex_lock(&priv->lock);
+//   if (ret < 0)
+//     {
+//       return (ssize_t)ret;
+//     }
 
-  if (stm32h5_wait_for_last_operation(priv))
-    {
-      ret = -EIO;
-      goto exit_with_lock;
-    }
+//   if (stm32h5_wait_for_last_operation(priv))
+//     {
+//       ret = -EIO;
+//       goto exit_with_lock;
+//     }
 
-  /* Get flash ready and begin erasing single block */
+//   /* Get flash ready and begin erasing single block */
 
-  stm32h5_unlock_flash(priv);
+//   stm32h5_unlock_flash(priv);
 
-  stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, 0, FLASH_CR_SER);
-  stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, FLASH_CR_SNB_MASK,
-                            FLASH_CR_SNB(block - priv->stblock));
+//   stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, 0, FLASH_CR_SER);
+//   stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, FLASH_CR_SNB_MASK,
+//                             FLASH_CR_SNB(block - priv->stblock));
 
-  stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, 0, FLASH_CR_START);
+//   stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, 0, FLASH_CR_START);
 
-  /* Wait for erase operation to complete */
+//   /* Wait for erase operation to complete */
 
-  if (stm32h5_wait_for_last_operation(priv))
-    {
-      ret = -EIO;
-      goto exit_with_unlock;
-    }
+//   if (stm32h5_wait_for_last_operation(priv))
+//     {
+//       ret = -EIO;
+//       goto exit_with_unlock;
+//     }
 
-  stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, FLASH_CR_SER, 0);
-  stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, FLASH_CR_SNB_MASK,
-                            0);
+//   stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, FLASH_CR_SER, 0);
+//   stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, FLASH_CR_SNB_MASK,
+//                             0);
 
-  ret = 0;
+//   ret = 0;
 
-  up_invalidate_dcache(block_address, block_address + FLASH_SECTOR_SIZE);
+//   up_invalidate_dcache(block_address, block_address + FLASH_SECTOR_SIZE);
 
-exit_with_unlock:
-  stm32h5_lock_flash(priv);
+// exit_with_unlock:
+//   stm32h5_lock_flash(priv);
 
-exit_with_lock:
-  nxmutex_unlock(&priv->lock);
+// exit_with_lock:
+//   nxmutex_unlock(&priv->lock);
 
-  /* Verify */
+//   /* Verify */
 
-  if (ret == 0 &&
-      stm32h5_israngeerased(block_address, up_progmem_erasesize(block)) == 0)
-    {
-      ret = up_progmem_erasesize(block); /* success */
-    }
-  else
-    {
-      ret = -EIO; /* failure */
-    }
+//   if (ret == 0 &&
+//       stm32h5_israngeerased(block_address, up_progmem_erasesize(block)) == 0)
+//     {
+//       ret = up_progmem_erasesize(block); /* success */
+//     }
+//   else
+//     {
+//       ret = -EIO; /* failure */
+//     }
 
-  return ret;
+//   return ret;
 }
 
 ssize_t up_progmem_write(size_t addr, const void *buf, size_t count)
 {
-  struct stm32h5_flash_priv_s *priv;
-  uint32_t     *fp;
-  uint32_t     *rp;
-  uint32_t     *ll        = (uint32_t *)buf;
-  size_t       faddr;
-  size_t       written    = count;
-  int          ret;
-  const size_t pagesize   = up_progmem_pagesize(0); /* 256 bit, 32 bytes per page */
-  const size_t llperpage  = pagesize / sizeof(uint32_t);
-  size_t       pcount     = count / pagesize;
-  uint32_t     sr;
+  #warning "(In development) up_progmem_write() is not implemented."
+//   struct stm32h5_flash_priv_s *priv;
+//   uint32_t     *fp;
+//   uint32_t     *rp;
+//   uint32_t     *ll        = (uint32_t *)buf;
+//   size_t       faddr;
+//   size_t       written    = count;
+//   int          ret;
+//   const size_t pagesize   = up_progmem_pagesize(0); /* 256 bit, 32 bytes per page */
+//   const size_t llperpage  = pagesize / sizeof(uint32_t);
+//   size_t       pcount     = count / pagesize;
+//   uint32_t     sr;
 
-  priv = stm32h5_flash_bank(addr);
+//   priv = stm32h5_flash_bank(addr);
 
-  if (priv == NULL)
-    {
-      return -EFAULT;
-    }
+//   if (priv == NULL)
+//     {
+//       return -EFAULT;
+//     }
 
-  /* Check for valid address range */
+//   /* Check for valid address range */
 
-  if (addr < priv->base ||
-      addr + count > priv->base + (STM32_FLASH_SIZE / 2))
-    {
-      return -EFAULT;
-    }
+//   if (addr < priv->base ||
+//       addr + count > priv->base + (STM32_FLASH_SIZE / 2))
+//     {
+//       return -EFAULT;
+//     }
 
-  ret = nxmutex_lock(&priv->lock);
-  if (ret < 0)
-    {
-      return (ssize_t)ret;
-    }
+//   ret = nxmutex_lock(&priv->lock);
+//   if (ret < 0)
+//     {
+//       return (ssize_t)ret;
+//     }
 
-  /* Check address and count alignment */
+//   /* Check address and count alignment */
 
-  DEBUGASSERT(!(addr % pagesize));
-  DEBUGASSERT(!(count % pagesize));
+//   DEBUGASSERT(!(addr % pagesize));
+//   DEBUGASSERT(!(count % pagesize));
 
-  if (stm32h5_wait_for_last_operation(priv))
-    {
-      written = -EIO;
-      goto exit_with_lock;
-    }
+//   if (stm32h5_wait_for_last_operation(priv))
+//     {
+//       written = -EIO;
+//       goto exit_with_lock;
+//     }
 
-  /* Get flash ready and begin flashing */
+//   /* Get flash ready and begin flashing */
 
-  stm32h5_unlock_flash(priv);
+//   stm32h5_unlock_flash(priv);
 
-  stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET,
-                            FLASH_CR_PSIZE_MASK, FLASH_CR_PSIZE);
+//   stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET,
+//                             FLASH_CR_PSIZE_MASK, FLASH_CR_PSIZE);
 
-  stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, 0, FLASH_CR_PG);
+//   stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, 0, FLASH_CR_PG);
 
-  for (ll = (uint32_t *)buf, faddr = addr; pcount;
-      pcount -= 1, ll += llperpage, faddr += pagesize)
-    {
-      fp = (uint32_t *)faddr;
-      rp = ll;
+//   for (ll = (uint32_t *)buf, faddr = addr; pcount;
+//       pcount -= 1, ll += llperpage, faddr += pagesize)
+//     {
+//       fp = (uint32_t *)faddr;
+//       rp = ll;
 
-      UP_MB();
+//       UP_MB();
 
-      /* Write 8 32 bit word and wait to complete */
+//       /* Write 8 32 bit word and wait to complete */
 
-      *fp++ = *rp++;
-      *fp++ = *rp++;
-      *fp++ = *rp++;
-      *fp++ = *rp++;
-      *fp++ = *rp++;
-      *fp++ = *rp++;
-      *fp++ = *rp++;
-      *fp++ = *rp++;
+//       *fp++ = *rp++;
+//       *fp++ = *rp++;
+//       *fp++ = *rp++;
+//       *fp++ = *rp++;
+//       *fp++ = *rp++;
+//       *fp++ = *rp++;
+//       *fp++ = *rp++;
+//       *fp++ = *rp++;
 
-      /* Data synchronous Barrier (DSB) just after the write operation. This
-       * will force the CPU to respect the sequence of instruction (no
-       * optimization).
-       */
+//       /* Data synchronous Barrier (DSB) just after the write operation. This
+//        * will force the CPU to respect the sequence of instruction (no
+//        * optimization).
+//        */
 
-      UP_MB();
+//       UP_MB();
 
-      if (stm32h5_wait_for_last_operation(priv))
-        {
-          written = -EIO;
-          goto exit_with_unlock;
-        }
+//       if (stm32h5_wait_for_last_operation(priv))
+//         {
+//           written = -EIO;
+//           goto exit_with_unlock;
+//         }
 
-      sr = stm32h5_flash_getreg32(priv, STM32_FLASH_SR1_OFFSET);
-      if (sr & (FLASH_SR_SNECCERR | FLASH_SR_DBECCERR))
-        {
-          stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET,
-                                    FLASH_CR_PG,
-                                    0);
+//       sr = stm32h5_flash_getreg32(priv, STM32_FLASH_SR1_OFFSET);
+//       if (sr & (FLASH_SR_SNECCERR | FLASH_SR_DBECCERR))
+//         {
+//           stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET,
+//                                     FLASH_CR_PG,
+//                                     0);
 
-          stm32h5_flash_modifyreg32(priv, STM32_FLASH_CCR1_OFFSET,
-                                    0, ~0);
-          ret = -EIO;
-          goto exit_with_unlock;
-        }
-    }
+//           stm32h5_flash_modifyreg32(priv, STM32_FLASH_CCR1_OFFSET,
+//                                     0, ~0);
+//           ret = -EIO;
+//           goto exit_with_unlock;
+//         }
+//     }
 
-  stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, FLASH_CR_PG, 0);
+//   stm32h5_flash_modifyreg32(priv, STM32_FLASH_CR1_OFFSET, FLASH_CR_PG, 0);
 
-  stm32h5_flash_modifyreg32(priv, STM32_FLASH_CCR1_OFFSET,
-                            0, ~0);
-exit_with_unlock:
-  stm32h5_lock_flash(priv);
+//   stm32h5_flash_modifyreg32(priv, STM32_FLASH_CCR1_OFFSET,
+//                             0, ~0);
+// exit_with_unlock:
+//   stm32h5_lock_flash(priv);
 
-  /* Verify */
+//   /* Verify */
 
-  if (written > 0)
-    {
-      for (ll = (uint32_t *)buf, faddr = addr, pcount = count / pagesize;
-          pcount; pcount -= 1, ll += llperpage, faddr += pagesize)
-        {
-          fp = (uint32_t *)faddr;
-          rp = ll;
+//   if (written > 0)
+//     {
+//       for (ll = (uint32_t *)buf, faddr = addr, pcount = count / pagesize;
+//           pcount; pcount -= 1, ll += llperpage, faddr += pagesize)
+//         {
+//           fp = (uint32_t *)faddr;
+//           rp = ll;
 
-          stm32h5_flash_modifyreg32(priv, STM32_FLASH_CCR1_OFFSET,
-                                    0, ~0);
-          if ((*fp++ != *rp++) ||
-              (*fp++ != *rp++) ||
-              (*fp++ != *rp++) ||
-              (*fp++ != *rp++) ||
-              (*fp++ != *rp++) ||
-              (*fp++ != *rp++) ||
-              (*fp++ != *rp++) ||
-              (*fp++ != *rp++))
-            {
-              written = -EIO;
-              break;
-            }
+//           stm32h5_flash_modifyreg32(priv, STM32_FLASH_CCR1_OFFSET,
+//                                     0, ~0);
+//           if ((*fp++ != *rp++) ||
+//               (*fp++ != *rp++) ||
+//               (*fp++ != *rp++) ||
+//               (*fp++ != *rp++) ||
+//               (*fp++ != *rp++) ||
+//               (*fp++ != *rp++) ||
+//               (*fp++ != *rp++) ||
+//               (*fp++ != *rp++))
+//             {
+//               written = -EIO;
+//               break;
+//             }
 
-          sr = stm32h5_flash_getreg32(priv, STM32_FLASH_SR1_OFFSET);
-          if (sr & (FLASH_SR_SNECCERR | FLASH_SR_DBECCERR))
-            {
-              written = -EIO;
-              break;
-            }
-        }
+//           sr = stm32h5_flash_getreg32(priv, STM32_FLASH_SR1_OFFSET);
+//           if (sr & (FLASH_SR_SNECCERR | FLASH_SR_DBECCERR))
+//             {
+//               written = -EIO;
+//               break;
+//             }
+//         }
 
-      stm32h5_flash_modifyreg32(priv, STM32_FLASH_CCR1_OFFSET,
-                                0, ~0);
-    }
+//       stm32h5_flash_modifyreg32(priv, STM32_FLASH_CCR1_OFFSET,
+//                                 0, ~0);
+//     }
 
-exit_with_lock:
-  nxmutex_unlock(&priv->lock);
-  return written;
+// exit_with_lock:
+//   nxmutex_unlock(&priv->lock);
+//   return written;
 }
 
 uint8_t up_progmem_erasestate(void)
 {
   return FLASH_ERASEDVALUE;
 }
+
+#endif /* CONFIG_ARCH_HAVE_PROGMEM */
